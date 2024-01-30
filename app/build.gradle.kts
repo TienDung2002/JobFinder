a28556b70fa3d3291c1acc0dc906386a5f43eac9
TienDung2002 <tiendungvu0504@gmail.com>
2024-01-30 18:43:53 +0700

    commit
    update file google service

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // google service firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+
     // Retrofit2
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
