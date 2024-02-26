c2929e338673359e22abaf7e7d71be5324de0c13
TienDung2002 <tiendungvu0504@gmail.com>
2024-02-26 16:26:13 +0700

    register(recruiter)
    thiết kế register recruiter và verify các trường

--- app/src/main/java/com/example/jobfinder/UI/Register/RecruiterRegisterActivity.kt
+++ app/src/main/java/com/example/jobfinder/UI/Register/RecruiterRegisterActivity.kt
@@ -4,8 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentManager
 import com.example.jobfinder.R
+import com.example.jobfinder.UI.Home.HomeActivity
 import com.example.jobfinder.Utils.Calendar
 import com.example.jobfinder.Utils.CalendarToggleState
 import com.example.jobfinder.Utils.PasswordToggleState
@@ -33,6 +35,39 @@ class RecruiterRegisterActivity : AppCompatActivity() {
 //            val showTextPlace = binding.foundationDay
 //            Calendar.showDatePickerDialog(getActivityFragmentManager(), isCalendarVisible, title, showTextPlace)
 //        }
+
+        // Đăng ký
+        binding.btnRegister.setOnClickListener{
+            val nameInput = binding.recName.text.toString()
+            val hotlineInput = binding.recHotline.text.toString()
+            val addressInput = binding.recAddress.text.toString()
+            val emailInput = binding.recEmail.text.toString()
+            val passInput = binding.password.text.toString()
+            val repassInput = binding.reEnterPass.text.toString()
+
+            val isValidName = nameInput.isNotEmpty()
+            val isValidHotline = hotlineInput.isNotEmpty()
+            val isValidAddress = addressInput.isNotEmpty()
+            val isValidEmail = emailInput.isNotEmpty() && VerifyField.isValidEmail(emailInput)
+            val isValidPassword = passInput.isNotEmpty()
+            val isValidRePassword = repassInput.isNotEmpty() && repassInput == passInput
+
+            binding.recName.error = if (isValidName) null else getString(R.string.error_invalid_name)
+            binding.recHotline.error = if (isValidHotline) null else getString(R.string.error_invalid_hotline)
+            binding.recAddress.error = if (isValidAddress) null else getString(R.string.error_invalid_addr)
+            binding.recEmail.error = if (isValidEmail) null else getString(R.string.error_invalid_email)
+            binding.password.error = if (isValidPassword) null else getString(R.string.error_pass)
+            binding.reEnterPass.error = if (isValidRePassword) null else getString(R.string.error_invalid_reEnterPass)
+
+            if (isValidName && isValidHotline && isValidAddress && isValidEmail && isValidPassword && isValidRePassword) {
+                val resultIntent = Intent()
+                setResult(Activity.RESULT_OK, resultIntent)
+                Toast.makeText(this, getString(R.string.register_success), Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, getString(R.string.error_email_pass), Toast.LENGTH_SHORT).show()
+            }
+        }
 
         // từ recruiter trở về login
         binding.returnbackLogin.setOnClickListener{
