91d21121cb2a2ed5e6a4e6e104b9b5dd0c8d56aa
Oriz1206 <phamtrungnguyen1206@gmail.com>
2024-03-14 16:35:16 +0700

    update profile
    đang làm profile sang fragment, chưa hoàn thiện

--- app/src/main/java/com/example/jobfinder/UI/UsersProfile/UserDetailActivity.kt
+++ app/src/main/java/com/example/jobfinder/UI/UsersProfile/UserDetailActivity.kt
@@ -1,12 +1,39 @@
 package com.example.jobfinder.UI.UsersProfile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jobfinder.R
+import com.example.jobfinder.UI.Register.SeekerRegisterActivity
+import com.example.jobfinder.Utils.PreventDoubleClick
+import com.example.jobfinder.databinding.ActivityUserDetailBinding
+import com.example.jobfinder.databinding.FragmentUserEditProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class UserDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserDetailBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_detail)
+        binding = ActivityUserDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // account detail
+
+        val userType = intent.getStringExtra("user_type")
+        binding.profileAccount.setOnClickListener {
+            if (PreventDoubleClick.checkClick()){
+                if(userType == "NUser"){
+                    val intent = Intent(this, UserEditProfile::class.java)
+                    startfr(intent)
+                } else if (userType == "BUser"){
+                    val intent = Intent(this, RecruiterEditProfile::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+
     }
+
 }
