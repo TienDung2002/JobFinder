2815244bade359117402cb03eeefa64e38e31d5c
TienDung2002 <tiendungvu0504@gmail.com>
2024-03-21 16:55:19 +0700

    update (NewJobActivity)
    thiết kế bố cục cho NewJobActivity

--- app/src/main/java/com/example/jobfinder/UI/Jobs/NewJobActivity.kt
+++ app/src/main/java/com/example/jobfinder/UI/Jobs/NewJobActivity.kt
@@ -1,12 +1,26 @@
 package com.example.jobfinder.UI.Jobs
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jobfinder.R
+import com.example.jobfinder.databinding.ActivityNewJobBinding
 
 class NewJobActivity : AppCompatActivity() {
+    lateinit var binding: ActivityNewJobBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_job)
+        binding = ActivityNewJobBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // nút back về home trên màn hình
+        binding.backButton.setOnClickListener {
+            val resultIntent = Intent()
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
     }
+
+
 }
