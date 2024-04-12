e07143bd9d3ade796ee1b3592226766ac5915a6d
MaiManh15 <sorryfornoob15@gmail.com>
2024-04-12 14:48:42 +0700

    buser_detail_info

--- app/src/main/java/com/example/jobfinder/UI/JobDetails/RecruiterJobDetailActivity.kt
+++ app/src/main/java/com/example/jobfinder/UI/JobDetails/RecruiterJobDetailActivity.kt
@@ -6,18 +6,23 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.jobfinder.Datas.Model.JobModel
 import com.example.jobfinder.Datas.Model.NotificationsRowModel
 import com.example.jobfinder.R
 import com.example.jobfinder.UI.Applicants.ActivityApplicantsList
 import com.example.jobfinder.UI.PostedJob.PostedJobViewModel
+import com.example.jobfinder.UI.UserDetailInfo.BUserDetailInfoActivity
+import com.example.jobfinder.UI.UsersProfile.ProfileViewModel
 import com.example.jobfinder.Utils.GetData
+import com.example.jobfinder.Utils.RetriveImg
 import com.example.jobfinder.databinding.ActivityRecruiterJobDetailBinding
 import com.google.firebase.database.*
 
 class RecruiterJobDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecruiterJobDetailBinding
     private val viewModel: PostedJobViewModel by viewModels()
+    private lateinit var imgViewModel: ProfileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +30,7 @@ class RecruiterJobDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
         val job = intent.getParcelableExtra<JobModel>("job")
 
+        imgViewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
 
         binding.animationView.visibility = View.VISIBLE
         binding.detailJobScrollView.visibility = View.GONE
@@ -43,6 +49,8 @@ class RecruiterJobDetailActivity : AppCompatActivity() {
             binding.jobDetailAddress.text= job.address
             binding.jobDetailDes.text= job.jobDes
 
+            RetriveImg.retrieveImage(job.BUserId.toString(), binding.buserLogo, this@RecruiterJobDetailActivity, imgViewModel)
+
             binding.detailJobScrollView.visibility = View.VISIBLE
             binding.animationView.visibility = View.GONE
 
@@ -59,6 +67,12 @@ class RecruiterJobDetailActivity : AppCompatActivity() {
             binding.appliedListBtn.setOnClickListener {
                 val intent = Intent(this, ActivityApplicantsList::class.java)
                 intent.putExtra("job_id", job.jobId)
+                startActivity(intent)
+            }
+
+            binding.buserLogo.setOnClickListener{
+                val intent = Intent(this, BUserDetailInfoActivity::class.java)
+                intent.putExtra("uid", job.BUserId)
                 startActivity(intent)
             }
         }
