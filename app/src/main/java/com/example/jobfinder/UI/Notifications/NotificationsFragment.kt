b0b2bc72061bbd6e6032ad7aa8e5204c53168ca0
TienDung2002 <tiendungvu0504@gmail.com>
2024-06-08 16:48:50 +0700

    update (xml)
    - đổi text sang icon notify
    - fix margin, padding support, wallet

--- app/src/main/java/com/example/jobfinder/UI/Notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/jobfinder/UI/Notifications/NotificationsFragment.kt
@@ -34,7 +34,6 @@ class NotificationsFragment : Fragment() {
         }
 
         binding.deleteAll.setOnClickListener{
-
             viewModel.deleteAllNoti()
             viewModel.fetchNotificationsFromFirebase()
         }
